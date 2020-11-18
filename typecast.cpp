af47ceaa087a69c26b2b5bd15f1a4e8b63d29076
neonstark9 <neonstark99@gmail.com>
2020-11-18 20:51:38 +0530

    typecast

---
+++ typecast.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x=100;
+    double y=double(x); //normal c style
+    double z=(double)x; //c++ style
+    cout<<x<<" "<<y<<" "<<z;
+}
